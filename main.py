e5a91c5f8f60fa63857f70e732db1252ce92da48
lac2860 <43333590+lac2860@users.noreply.github.com>
2019-06-26 02:59:47 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+first_name =  input("What is your first name? ") 
+
+
+print("Hello,",first_name)
+if first_name == "Craig":
+    print(first_name,"is learning python")
+else:
+    print("You should totally learn Python, {}!".format(first_name))
+print("Have a great day {}!".format(first_name))
